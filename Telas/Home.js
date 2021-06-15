ba5adc8de7510ff489a2454d8b52c2629f3eb071
souzavini <viniciuscarr@hotmail.com>
2021-06-15 19:19:39 -0300

    Adicionando conexao com firebase

--- Telas/Home.js
+++ Telas/Home.js
@@ -8,7 +8,9 @@ import {
   View,
 } from "react-native";
 import CardQuote from "../components/card";
-
+import { useList } from "react-firebase-hooks/database";
+import databaseFactory from "../factories/databasefactory.js";
+/*
 const list = [
     { id: 1, author: 'Platão', profileImg: 'https://i2.wp.com/filosofiapop.com.br/wp-content/uploads/2015/06/plato-1.jpg', image:'https://blog.emania.com.br/wp-content/uploads/2019/01/como-tirar-foto-de-cachorro-vinculo-1024x598.jpg' },
     { id: 2, author: 'Marco Antonio', profileImg: 'https://www2.ufjf.br/pgcc/wp-content/uploads/sites/181/2008/07/Marco.jpg', image:'https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg'},
@@ -16,25 +18,29 @@ const list = [
     { id: 4, author: 'Chil do Piseiro', profileImg: 'https://gcmais.com.br/wp-content/uploads/2020/10/piseiro.jpg', image:'https://s2.glbimg.com/slaVZgTF5Nz8RWqGrHRJf0H1PMQ=/0x0:800x450/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/U/e/NTegqdSe6SoBAoQDjKZA/cachorro.jpg'},
     { id: 5, author: 'Chil do Piseiro', profileImg: 'https://gcmais.com.br/wp-content/uploads/2020/10/piseiro.jpg', image:'https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg'},
     { id: 6, author: 'Chil do Piseiro', profileImg: 'https://gcmais.com.br/wp-content/uploads/2020/10/piseiro.jpg', image:'https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg'}
-  ]
+  ]*/
   
 
-const Home = ({}) =>{
+const Home = () =>{
+  const [cards, loading, erro] = useList(
+    databaseFactory.createDatabaseService("firebase")
+  );
+
     return(
+      console.log(cards),
         <SafeAreaView style={styles.container}>
       <StatusBar
         animated={true}
         backgroundColor={"#fff"}
       />
       <FlatList
-        data={list}
+        data={cards}
         keyExtractor={item => item.id}
         renderItem={({ item }) =>
           <CardQuote 
-          author={item.author}
-          text={item.text}
-            profileImg={item.profileImg}
-            image={item.image} />
+          author={item.val().author}
+            profileImg={item.val().profileImg}
+            image={item.val().image} />
         }
       >
       </FlatList>
@@ -52,4 +58,3 @@ const styles = StyleSheet.create({
   });
 
 export default Home;
-
