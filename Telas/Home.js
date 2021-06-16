22c0000d68f5c3a271972225e45e44ac7d73ec2b
Thiago Silva <thsilvar@gmail.com>
2021-06-15 21:02:01 -0300

    Update Home.js

--- Telas/Home.js
+++ Telas/Home.js
@@ -10,7 +10,7 @@ import {
 import CardQuote from "../components/card";
 import { useList } from "react-firebase-hooks/database";
 
-import databaseFactory from "../factories/databasefactory.js";
+import databaseFactory from "../factories/databaseFactory";
 
 /*
 const list = [
